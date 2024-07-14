0e30549e6030d855664efdb7cb84be22dbd4931e
Advika Sinha <152763494+advikasinha@users.noreply.github.com>
2024-07-14 20:59:40 +0530

    Revert "Update app.py"
    This reverts commit 640c7f8e160048cc17132decde05203541cedbf3.

--- app.py
+++ app.py
@@ -101,11 +101,9 @@ def main():
     # File uploaders
     col1, col2 = st.columns(2)
     with col1:
-        st.markdown('<p style="color: #F05524; font-style: italic; margin-top: 10px;">Upload your Silhouette</p>', unsafe_allow_html=True)
-        content_file = st.file_uploader('', type=["png", "jpg", "jpeg"])  # Empty string for label
+        content_file = st.file_uploader('Upload your Silhouette', type=["png", "jpg", "jpeg"], help='<p style="color: #F05524; font-style: italic;">Upload your Silhouette</p>')
     with col2:
-        st.markdown('<p style="color: #F05524; font-style: italic; margin-top: 10px;">Upload your Style</p>', unsafe_allow_html=True)
-        style_file = st.file_uploader('', type=["png", "jpg", "jpeg"])  # Empty string for label
+        style_file = st.file_uploader('Upload your Style', type=["png", "jpg", "jpeg"], help='<p style="color: #F05524; font-style: italic;">Upload your Style</p>')
 
     if content_file and style_file:
         if st.button("Design It!", key="design_button", help="Click to design your image"):
