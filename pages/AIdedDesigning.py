a8fd8db3a10cf1ad0c7aa8a7d35cb17666948456
Advika Sinha <152763494+advikasinha@users.noreply.github.com>
2024-07-15 20:01:01 +0530

    centering

--- pages/AIdedDesigning.py
+++ pages/AIdedDesigning.py
@@ -89,7 +89,7 @@ def main():
         prompt=f'Fashion illustration of {pattern.lower()} {color} colored {cloth_type.lower()}, high quality, detailed design, fashion illustration, abstract, professional, vibrant colors, artistic style, elegant, flowing fabric, intricate details, beautiful, modern, colorful, fashion design, stylish, high-res, intricate patterns, fashion illustration style, vibrant lighting'
         image_url = generate_image(prompt)
         if image_url is None:
-            st.markdown("""<div class='lorem-ipsum'> <p style="font-size: 16px; line-height: 1.6; color: #333;">Explore our previous creations by the s prompt meanwhile: </p>  
+            st.markdown("""<div class='lorem-ipsum'> <p style="font-size: 16px; line-height: 1.6; color: #333;">Explore our previous creations by similar prompts meanwhile: </p>  
                 </div>""", unsafe_allow_html=True)
 
             current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -102,8 +102,9 @@ def main():
                     for image_file in image_files:
                         image_path = os.path.join(image_folder, image_file)
                         img = Image.open(image_path)
-                        img.thumbnail((200, 200))  # Resize image to 500px max
-                        image_container.image(img, width =300, use_column_width=False)
+                        col1, col2, col3 = st.columns([1,2,1])
+                        with col2:
+                            image_container.image(img, use_column_width=True)
                         time.sleep(3)  # Display each image for 3 seconds
             else:
                 st.write("No images found in the DALL-E-gen folder.")
